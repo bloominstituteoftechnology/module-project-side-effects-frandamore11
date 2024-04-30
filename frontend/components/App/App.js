428a962c19bcb995ce1b45e991d4b9cf5acf30dc
Francesco Damore <frandamore11@gmail.com>
2024-04-29 22:23:13 -0600

    finished

--- frontend/components/App/App.js
+++ frontend/components/App/App.js
@@ -15,7 +15,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+  const [posts, setPosts] = useState(data)
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -28,12 +28,19 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(posts.map(post => {
+      if (post.id !== postId) return post
+      let updatedPost = { ...post, likes: post.likes +1 }
+      return updatedPost
+    }))
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      <SearchBar />
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <Posts likePost={likePost} posts={posts}/>
     </div>
   );
 };
